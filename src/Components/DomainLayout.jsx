a2c8727418a8a7e3f1a2e56f83dcc636a335a371
Navanit Jha <Devashri.Mahindrakar@harbingergroup.com>
2024-11-19 16:19:47 +0530

    Remove logs

--- src/Components/DomainLayout.jsx
+++ src/Components/DomainLayout.jsx
@@ -36,7 +36,7 @@ const DomainLayout = () => {
   const fetchMockups = (userId) => {
     axios
       .get(
-        `https://hxstudiofileupload.azurewebsites.net/api/FileUploadAPI/${userId}/mockups`
+        `https://hxstudiofileuploadv1.azurewebsites.net/api/FileUploadAPI/${userId}/mockups`
       )
       .then((response) => {
         const fetchedMockups = response.data.map((mockup) => ({
@@ -62,7 +62,7 @@ const DomainLayout = () => {
     setIsLoadingFavorites(true);
     try {
       const response = await axios.get(
-        `https://hxstudiofileupload.azurewebsites.net/api/FileUploadAPI/${user.id}/likes`
+        `https://hxstudiofileuploadv1.azurewebsites.net/api/FileUploadAPI/${user.id}/like`
       );
       const favoriteIds = response.data.map(
         (favorite) => favorite.mockupGroupId
@@ -107,7 +107,7 @@ const DomainLayout = () => {
     try {
       // Add to favorites with true as payload
       await axios.post(
-        `https://hxstudiofileupload.azurewebsites.net/api/FileUploadAPI/${user.id}/like/${mockupId}`,
+        `https://hxstudiofileuploadv1.azurewebsites.net/api/FileUploadAPI/${user.id}/like/${mockupId}`,
         true,
         {
           headers: {
@@ -134,7 +134,7 @@ const DomainLayout = () => {
     setShowFavorites(false); // Reset showFavorites state
     axios
       .get(
-        `https://hxstudiofileupload.azurewebsites.net/api/FileUploadAPI/searchByDomain?userId=${user.id}&domainName=${domainName}`
+        `https://hxstudiofileuploadv1.azurewebsites.net/api/FileUploadAPI/searchByDomain?userId=${user.id}&domainName=${domainName}`
       )
       .then((response) => {
         const filteredMockups = response.data.map((mockup) => ({
@@ -159,7 +159,7 @@ const DomainLayout = () => {
   // const handleDelete = (mockup) => {
   //   axios
   //     .delete(
-  //       `https://hxstudiofileupload.azurewebsites.net/api/FileUploadAPI/delete/${mockup.id}`
+  //       `https://hxstudiofileuploadv1.azurewebsites.net/api/FileUploadAPI/delete/${mockup.id}`
   //     )
   //     .then(() => {
   //       setMockups((prevMockups) =>
@@ -196,9 +196,9 @@ const DomainLayout = () => {
     setSortOption(sort);
     let apiUrl;
     if (sort === "Alphabetically") {
-      apiUrl = `https://hxstudiofileupload.azurewebsites.net/api/FileUploadAPI/alphabetical?userId=${user.id}`;
+      apiUrl = `https://hxstudiofileuploadv1.azurewebsites.net/api/FileUploadAPI/alphabetical?userId=${user.id}`;
     } else {
-      apiUrl = `https://hxstudiofileupload.azurewebsites.net/api/FileUploadAPI/recent?userId=${user.id}`;
+      apiUrl = `https://hxstudiofileuploadv1.azurewebsites.net/api/FileUploadAPI/recent?userId=${user.id}`;
     }
 
     axios
